9f4fa218c02e8c0a6842013c65871aa0e6501652
Adilson <dimmu.ma@hotmail.com>
2022-03-17 18:33:28 -0300

    Primeiro Commit

---
+++ import qrcode.py
@@ -0,0 +1,12 @@
+#O intuito do código abaixo é gerar de maneira simples e com poucas linhas um QRcode baseado na biblioteca Qrcode do Python#
+
+import qrcode
+
+#(pip install qrcode) Biblioteca utilizada para fazer a importação do gerador de QRcode.#
+#Para instalar a mesma, abrir novo terminal e colar o "pip install qrcode"
+
+img = qrcode.make("Url da pagina")
+#refere-se ao tipo de imagem criada e o endereço da pagina propriamente dita#
+
+img.save("nome da pagina.jpg")
+#Nome de como o arquivo de imagem gerado, será salvo# 
